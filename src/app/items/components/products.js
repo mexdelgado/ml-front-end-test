c14df44b24fdfd57c0f886f76761eb46dcef8976
Maximiliano Delgado <mdelgado@woonky.com>
2017-10-12 23:45:12 -0300

    show item

--- src/app/items/components/products.js
+++ src/app/items/components/products.js
@@ -18,11 +18,11 @@ class Products extends Component {
       <div className="products">
 
         <div className="row">
-            <div className="col-xs-12">
+          <div className="col-xs-12">
               <ol className="breadcrumb BreadcrumbCategories">
-                {products.categories && products.categories.map( category =>
-                  <li key={category}><a href="javascript:void(0);">{category}</a></li>
-                )}
+                {products.categories && products.categories.map((category, index) => (
+                    <li key={index}><a href="javascript:void(0);">{category}</a></li>
+                ))}
               </ol>
           </div>
         </div>
@@ -59,8 +59,33 @@ class Products extends Component {
     return(
       <div className="product">
 
-        <div className="productList">
-          <p>{product.item.title}</p>
+        <div className="productInfo">
+          <div className="row">
+
+            <div className="col-md-7">
+              <img className="product-img" src={product.item.picture} width="100%"/>
+            </div>
+
+            <div className="col-md-4 col-md-offset-1">
+              <div className="content-info-product">
+                <p className="condition-product">
+                  {product.item.condition} - {product.item.sold_quantity} vendidos
+                </p>
+                <h3 className="title">{product.item.title}</h3>
+                <h1 className="price">$ {product.item.price.amount}</h1>
+
+                <button className="btn btn-default btn-block">Comprar</button>
+              </div>
+            </div>
+
+            <div className="col-md-9">
+              <div className="content-detail-product">
+                <h2 className="title">Descripción del producto</h2>
+                <div className="description" dangerouslySetInnerHTML={{ __html: product.item.description }}></div>
+              </div>
+            </div>
+
+          </div>
         </div>
 
       </div>
